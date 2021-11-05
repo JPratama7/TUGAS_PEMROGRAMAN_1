3e77a748552debd38171250dba8173bd5f44a460
JPratama7 <josepratama080@gmail.com>
2021-11-05 17:14:08 +0800

    Upload Tugas

---
+++ P1_1_1204004/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1_1_1204004
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Console.WriteLine("Hello World from Visual Studio, I'm 1204004");
+        }
+    }
+}
